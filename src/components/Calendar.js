a573f193a8cd859493b432a9b008714d0ac05cfb
SimpllleUser <pecheritsa.vv@gmail.com>
2020-06-26 19:59:29 +0300

    Add all data for calendar

---
+++ src/components/Calendar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import moment from "moment";
+
+const today = moment;
+
+const test = () => {
+  console.log("WORK");
+};
+const initMonth = () => {
+  const MonthYear = today().format("MM-YYYY");
+  let month = [];
+  let max_day = today(`2020-9`, "YYYY-MM").daysInMonth();
+  for (let i = 0; i < max_day; i++) {
+    let num = i + 1;
+    let name = today(`${num}-${MonthYear}`, "DD-MM-YYYY").format("dddd");
+    let data_day = { num, name };
+    month.push(data_day);
+  }
+  return month;
+};
+
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thus", "Fri", "Sut", "Sun"] 
+
+const listDay = initMonth().map((day) => (
+  <div key={day.num}>
+    {day.num}
+    {/* {day.name} */}
+  </div>
+));
+
+const listWeek = daysOfWeek.map((day) => (
+  <div key={day}>{day}</div>
+))
+
+const Calendar = () => {
+  return (
+    <div>
+      <h1> Calendar </h1> <div className="month">
+         {listWeek}
+         {listDay}
+          </div>
+      {console.log(initMonth())}
+    </div>
+  );
+};
+
+export default Calendar;
--- src/pages/ListPosts.js
+++ src/pages/ListPosts.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Posts from "../components/Posts";
+import Calendar from "../components/Calendar";
 
 const ListPosts = () => (
-    <div>
-        <h2>Lists posts</h2>
-        <Posts/>
-
+  <div>
+    <h2>Lists posts</h2>
+    <div className="row">
+      <div className="col-6">
+        <Posts />
+      </div>
+      <div className="col-6">
+        <Calendar />
+      </div>
     </div>
-)
+  </div>
+);
 
-export default ListPosts
+export default ListPosts;
