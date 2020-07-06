a12572509e55f6718c6dbe9a09e1b2016fe5ed1b
unknown <pecheritsa.vv@gmail.com>
2020-07-06 12:11:16 +0300

    Fortmat date calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,110 +1,143 @@
-import React, { useState } from "react";
+import React from "react";
 import { ArrowLeft, ArrowRight } from "react-feather";
 import moment from "moment";
 
-const today = moment;
+// const today = moment;
 
-const Calendar = () => {
-  let [selectYear, setYear] = useState(moment().format("YYYY"));
-  let [selectMonth, setMonth] = useState(moment().format("M"));
-  const daysOfWeek = ["Mon", "Tue", "Wed", "Thus", "Fri", "Sut", "Sun"];
-  const size = 18;
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+       daysOfWeek : ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'],
+       prevMonthDay: (lenthDays) => {
+         let day = 0
+         let days = []
+         while(day < lenthDays){
+           day+=1
+           days.push(day)
+         }
+         if(days.length > 0){
+          return days
+        }
+        return
+       },
+      selectMonth:moment().format("M"),
+      selectYear:moment().format("YYYY"),
+      month: () => {
+        let month = [];
+        let max_day = moment(
+          `${this.state.selectYear}-${this.state.selectMonth}`,
+          "YYYY-MM"
+        ).daysInMonth();
+        for (let i = 0; i < max_day; i++) {
+          let num = i + 1;
+          let name = moment(`${this.state.selectYear}-${this.state.selectMonth}-${num}`).format('dddd')
+          let data_day = { num, name };
+          month.push(data_day);
+        }
+        let firstElem = month[0].name
+        let lenthEmptDay = this.state.daysOfWeek.findIndex(m => m == firstElem )
+        let prevMonthDay = this.state.prevMonthDay(lenthEmptDay)
+        return  prevMonthDay ? [...prevMonthDay, ...month] : month
+      },
+    };
+  }
 
-
-  const initMonth = () => {
-    const MonthYear = today().format("MM-YYYY");
-    let month = [];
-    let max_day = today(
-      `${selectMonth}-${selectMonth}`,
-      "YYYY-MM"
-    ).daysInMonth();
-    for (let i = 0; i < max_day; i++) {
-      let num = i + 1;
-      let name = today(`${num}-${MonthYear}`, "DD-MM-YYYY").format("dddd");
-      let data_day = { num, name };
-      month.push(data_day);
-    }
-    return month;
-  };
-
-  const setBorder = (name) => {
-    let border = name === "Sunday" ? "danger" : "primary";
-    return `border border-${border}`;
-  };
-
-  const selectDay = (day) => {
+  selectDay = (day) => {
     console.log("DAY", day);
   };
 
-  const listDay = initMonth().map((day) => (
-    <div
-      className={"day " + setBorder(day.name)}
-      key={day.num}
-    >
-      <div className="dayNum">{day.num}</div>
-    </div>
-  ));
+   nextMonth = () => {
+     let selectMonth = +this.state.selectMonth
+     let selectYear = +this.state.selectYear
+    if (selectMonth >= 12) {
+      selectMonth = 0
+      selectYear++
+    }
+    selectMonth++
+    this.setState({selectMonth, selectYear})
+ }
+ prevMonth = () => {
+  let selectMonth = +this.state.selectMonth
+  let selectYear = +this.state.selectYear
+    if (selectMonth <= 1) {
+      selectMonth = 13;
+      selectYear--
+        }
+    selectMonth--
+    this.setState({selectMonth, selectYear})
+  };
 
-  const listWeek = daysOfWeek.map((day) => (
-    <div className="week" key={day}>
+ nextYear = () => {
+  let selectYear = +this.state.selectYear
+  selectYear++
+  this.setState({selectYear})
+};
+ prevYear = () => {
+  let selectYear = +this.state.selectYear
+  selectYear--
+  if(selectYear <= 1){
+    selectYear = 22020
+  }
+  this.setState({selectYear})
+  };
+
+  nameMonth = (day) => {
+    return moment(`${this.state.selectMonth}`).format("MMMM");
+  };
+
+  render() {
+    var size = 48
+    // 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Tuesday','Sunday'
+    // const daysOfWeek = {}
+      const listWeek = this.state.daysOfWeek.map((day) => (
+        <div className="week" key={day}>
       {day}
     </div>
   ));
 
-  const nextMonth = () => {
-    if (selectMonth >= 12) {
-      selectMonth = 0;
-      setYear(+selectYear + 1);
-    }
-    setMonth(+selectMonth + 1);
+    const setBorder = (name) => {
+    let border = name === "Sunday" ? "danger" : "primary";
+    return `border border-${border}`;
   };
-  const prevMonth = () => {
-    if (selectMonth <= 1) {
-      selectMonth = 13;
-      setYear(+selectYear - 1);
-    }
-    setMonth(+selectMonth - 1);
-  };
-
-  const nextYear = () => {
-    setYear(+selectYear + 1);
-  };
-  const prevYear = () => {
-    setYear = +selectYear <= 1 ? setYear(2020) : setYear(+selectYear - 1);
-  };
-
-  const nameMonth = (day) => {
-    return today(`${selectMonth}`).format("MMMM");
-  };
-
-  return (
-    <div>
-      <h1> Calendar {selectDay()}</h1>
+    const listDay = this.state.month().map((day) => (
+    <div
+      className={"day " + setBorder(day.name)}
+      key={day.num}
+      onClick = {() => {this.selectDay(day)}}
+    >
+      <div className="dayNum">{day.num}</div>
+    </div>
+  ));
+    return (
+      <div>
+         <h1 > Calendar </h1>
       <div className="navigation-calendar year-header">
-        <div className="prev year" onClick={prevYear}>
+        <div className="prev year" onClick={this.prevYear}>
           <ArrowLeft size={size} />
         </div>
-        <div className="name-year">{selectYear}</div>
-        <div className="next year" onClick={nextYear}>
+        <div className="name-year">{this.state.selectYear}</div>
+        <div className="next year" onClick={this.nextYear}>
           <ArrowRight size={size} />
         </div>
       </div>
       <div className="navigation-calendar month-header">
-        <div className="prev month" onClick={prevMonth}>
+        <div className="prev month" onClick={this.prevMonth}>
           <ArrowLeft size={size} />
         </div>
-        <div className="name-month">{nameMonth()}</div>
-        <div className="next month" onClick={nextMonth}>
+        <div className="name-month">{this.nameMonth()}</div>
+        <div className="next month" onClick={this.nextMonth}>
           <ArrowRight size={size} />
         </div>
       </div>
       <div className="month">
         {listWeek}
         {listDay}
+      </div> 
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Calendar;
