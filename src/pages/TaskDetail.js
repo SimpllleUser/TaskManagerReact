815ca7fa2efe6f80d787b242ebd14bb4b022cbd2
unknown <pecheritsa.vv@gmail.com>
2020-07-22 14:13:25 +0300

    Fix redirect to detail page

--- src/pages/TaskDetail.js
+++ src/pages/TaskDetail.js
@@ -1,55 +1,77 @@
 import React from "react";
 import moment from "moment"
 import { Redirect, NavLink } from "react-router-dom";
-import { connect } from "react-redux";
-import { useDispatch } from "react-redux";
+// import { connect } from "react-redux";
 import { deleteTask } from "../redux/actions";
 import { Edit2, Trash2 } from "react-feather";
 import SelectorElement from "../components/SelectorElement";
+import axios from "axios"
 
-const TaskDetail = (props) => {
-  const dispatch = useDispatch();
+class TaskDetail extends React.Component {
 
-  const task = props.tasks.find((p) => p.id === props.match.params.id);
-  if (task === undefined) {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      task:{}
+    }
+  }
+
+  componentWillMount(){
+    let r = this
+    let task = axios.get('http://localhost:8080/api/tasks/'+this.props.match.params.id).then(
+      response => {
+        let task = response.data
+        this.setState({task})
+      }
+    )
+    
+    
+  }
+
+
+
+  render(){
+  const size = 20;
+  if (this.state.task === undefined) {
     return <Redirect to="/" />;
   }
-  const size = 20;
-
   return (
     <div className="jumbotron" id="task-detail">
+      {console.log('11111',this.state.task)}
       <div className="task-body">
-        <h3 className="title display-4"> {task.title} </h3>
+        <h3 className="title display-4"> {this.state.task.title} </h3>
         <hr/>
-        <p className="description my-4"> {task.description} </p>
+        <p className="description my-4"> {this.state.task.description} </p>
         <div className="options">
-          <SelectorElement data={task.priority} type="priority" />
-          <SelectorElement data={task.status} type="status" />
+          {/* <SelectorElement data={this.state.task.priority} type="priority" />
+          <SelectorElement data={this.state.task.status} type="status" /> */}
         </div>
         </div>
 
         <div className="actions">
-          <NavLink className="edit-detail" to={`/edit-task/${task.id}`}>
+          <NavLink className="edit-detail" to={`/edit-task/${this.state.task.id}`}>
             <Edit2 className="text-secondary" size={size} />
           </NavLink>
           <div
             className="text-secondary trash-detail"
             onClick={() => {
-              dispatch(deleteTask(task.id));
+              deleteTask(this.state.task.id);
             }}
           >
             <Trash2 size={size} />
           </div>
         </div>
-        <small className="date-created"> {moment(task.createdAt).format("DD-MMMM-YYYY")} </small>
+        <small className="date-created"> {moment(this.state.task.createdAt).format("DD-MMMM-YYYY")} </small>
     </div>
   );
-};
+}
+}
 
 // export default EditTask;
-const mapStateToProps = (state) => {
-  return {
-    tasks: state.tasks.tasks,
-  };
-};
-export default connect(mapStateToProps, null)(TaskDetail);
+// const mapStateToProps = (state) => {
+//   return {
+//     tasks: state.tasks.tasks,
+//   };
+// };
+export default TaskDetail;
