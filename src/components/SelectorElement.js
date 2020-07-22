3caf35fe7c403f2fe2a61c355841093858cec46c
unknown <pecheritsa.vv@gmail.com>
2020-07-22 18:25:59 +0300

    Fix options in detail page after fix

--- src/components/SelectorElement.js
+++ src/components/SelectorElement.js
@@ -1,21 +1,23 @@
 import React from "react"
 
-const SelectorElement = (props) => {
-const data = {     "priority":  [
-        { name: "Low", value: 1, class: "success" },
-        { name: "Normal", value: 2, class: "warning" },
-        { name: "Highly", value: 3, class: "danger" },
-      ],
-      "status":  [
-        { name: "Open", value: 1, class: "primary" },
-        { name: "Inprogress", value: 2, class: "warning" },
-        { name: "Done", value: 3, class: "info" },
-      ]}
 
-      var select_element = data[props.type].find(elem => elem.name == props.data)
-      
-      
-    return select_element != undefined ?  <div className="selected-element-option">
+const data = {     "priority":  [
+  { name: "Low", value: 1, class: "success" },
+  { name: "Normal", value: 2, class: "warning" },
+  { name: "Highly", value: 3, class: "danger" },
+],
+"status":  [
+  { name: "Open", value: 1, class: "primary" },
+  { name: "Inprogress", value: 2, class: "warning" },
+  { name: "Done", value: 3, class: "info" },
+]}
+
+
+const SelectorElement = (props) => {
+
+  var select_element = data[props.type].find(elem => elem.name == props.data)
+
+    return select_element ?  <div className="selected-element-option">
       <div className={'badge badge-pill badge-'+select_element.class }> 
       </div>
       {select_element.name} 
