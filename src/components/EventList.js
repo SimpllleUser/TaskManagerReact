f6b5410716376f434405b2bc0310d92dc6500ce0
SimpllleUser <pecheritsa.vv@gmail.com>
2020-07-23 20:43:53 +0300

    Add filter event list

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,36 +1,54 @@
 import React from "react";
 import { connect } from "react-redux";
 import { getAllTasks } from "../redux/actions";
-import EventCard from "./EventCard"
+import EventCard from "./EventCard";
 import { NavLink } from "react-router-dom";
 
 class EventList extends React.Component {
 
-    componentDidMount() {
-        this.props.getAllTasks();
-    }
+  componentDidMount() {
+    this.props.getAllTasks();
+  }
 
-    render(){
-        return <div id="event-list">
-                <h1>Event list</h1>
-                {
-                    this.props.events.map((event) => (
-                        event.status ? <NavLink to={`/detail-task/${event.id}`} key={event.id}id="event-task"><EventCard key={event.id} event={event}></EventCard></NavLink> : <EventCard key={event.id} event={event}></EventCard> 
-                    ))
-                }
-            </div>
-    }
+  render() {
+    const filter_event = this.props.events.filter(
+      (e) => e.date === this.props.select_date
+    );
 
+    return (
+      <div id="event-list">
+        <h1> Список событий</h1>
+        {filter_event.map((event) =>
+          event.status ? (
+            <NavLink
+              to={`/detail-task/${event.id}`}
+              key={event.id}
+              id="event-task"
+            >
+              
+              <EventCard key={event.id} event={event}>
+                
+              </EventCard>
+            </NavLink>
+          ) : (
+            <EventCard key={event.id} event={event}>
+              
+            </EventCard>
+          )
+        )}
+      </div>
+    );
+  }
 }
 const mapDispatchToProps = {
-    getAllTasks
-  };
+  getAllTasks,
+};
 
 const mapStateToProps = (state) => {
-    return {
-        events: state.calendar.events.concat(state.tasks.tasks),
-        tasks: state.tasks.tasks
-    };
+  return {
+    events: state.calendar.events.concat(state.tasks.tasks),
+    tasks: state.tasks.tasks,
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventList);
