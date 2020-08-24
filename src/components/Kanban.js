c9c6c48e0c73645cff3621de88e4a7989edae54e
SimpllleUser <pecheritsa.vv@gmail.com>
2020-08-24 16:03:44 +0300

    Fix trmplate for create template kanban in task list

---
+++ src/components/Kanban.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { DragSource, DropTarget } from "react-dnd";
+
+const Kanban = () => {
+    return (<div>
+        <h1>Kanban</h1>
+        
+    </div>)
+}
+
+export default Kanban 
--- src/pages/TaskList.js
+++ src/pages/TaskList.js
@@ -1,9 +1,9 @@
-import React , { useState, useEffect}  from "react";
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { getAllTasks } from "../redux/actions";
 import { connect } from "react-redux";
 import Task from "../Task/Task";
-
+import { getElementError } from "@testing-library/react";
 
 // componentWillMount() {
 //   axios
@@ -13,32 +13,52 @@ import Task from "../Task/Task";
 //   });
 // }
 
-
-
 const TaskList = (props) => {
   let [tasks, setTasks] = useState([]);
   useEffect(() => {
-    axios
-    .get("http://localhost:8080/api/tasks")
-    .then((response) => {
-      setTasks(tasks = response.data)
-      props.getAllTasks(response.data)
+    axios.get("http://localhost:8080/api/tasks").then((response) => {
+      setTasks((tasks = response.data));
+      props.getAllTasks(response.data);
     });
-    }, [])
+  }, []);
 
-    //getAllTasks
+  //getAllTasks
+  const rowsByStatus = () => {
+    var elements = [
+      { name: "Open", tasks: [] },
+      { name: "Done", tasks: [] },
+      { name: "Inprogress", tasks: [] },
+    ];
 
-    const taskList = props.tasks ? props.tasks.map((task, index) => <Task task={task} key={index}/>) : ''
-    //.map((task, index) => <Task task={task} key={index}/>)
-
-  return (<div>
-    <h2> Список заданий </h2>
-    <div className="row">
-      <div className="col-12 tasks-list">
-        {taskList}
+    elements.forEach(
+      (el) => (el.tasks = props.tasks.filter((task) => task.status == el.name))
+    );
+    return elements;
+  };
+  // const taskList = props.tasks
+  //   ? props.tasks.map((task, index) => <Task task={task} key={index} />)
+  //   : "";
+  return (
+    <div id="task-dashboard">
+      <h2> Список заданий </h2>
+      <div className="row">
+        <div className="col-12 tasks-list">
+          {rowsByStatus().map((el, index) => (
+            <div className={'block-' + el.name} key={index}>
+              <h3 className={"el-name-" + el.name}>{el.name}</h3>
+              {console.log(el.name)}
+              <div className={"list-" + el.name}>
+                {el.tasks.map((task, index) => (
+                  <Task task={task} key={index} />
+                ))}
+              </div>
+            </div>
+          ))}
+          <div></div>
+        </div>
       </div>
     </div>
-  </div>)
+  );
 };
 
 const mapDispatchToProps = {
