2c47101b977e537ac190b49fbb8130771329e730
Loldf <pecheritsa.vv@gmail.com>
2020-10-08 17:27:47 +0300

    Add last change:

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Loader = () => (
-  <div className="spinner-border text-success" id="loader">
+  <div className="spinner-border text-danger" id="loader">
     <span className="sr-only"> Loading... </span>
   </div>
 );
