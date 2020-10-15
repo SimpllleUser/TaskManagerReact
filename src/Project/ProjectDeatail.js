efeb366b6076d28a7b988df463bb8c6ac09adb53
Loldf <pecheritsa.vv@gmail.com>
2020-10-15 09:48:13 +0300

    Fix detail page global task

--- src/Project/ProjectDeatail.js
+++ src/Project/ProjectDeatail.js
@@ -1,35 +1,47 @@
 import React, { useState, useEffect } from "react";
-import {useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { PlusSquare } from "react-feather";
 import { useParams } from "react-router-dom";
 import { useHttp } from "../hooks/http.hook";
 import { setGlobalTasks } from "../store/global_task/actions";
-import Modal from "../Modals/Modal"
-import GlobalTaskCard from "../GlobalTask/GlobalTaskCard"
-import GlobalTaskForm from "../GlobalTask/GlobalTaskForm"
+import Modal from "../Modals/Modal";
+import GlobalTaskCard from "../GlobalTask/GlobalTaskCard";
+import GlobalTaskForm from "../GlobalTask/GlobalTaskForm";
 
 const ProjectDetail = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   const [project, setProject] = useState({});
+  const [global_tasks, setGlobalTasks] = useState({});
+
   const { request, loading } = useHttp();
   useEffect(() => {
     const getProject = async () => {
-      const res = await request("http://localhost:8080/api/project/" + id);
-      dispatch(setGlobalTasks(res.global_tasks || []))
-      setProject(res);
+      const project = await request("http://localhost:8080/api/project/" + id);
+      setProject(project);
+    };
+    const getGlobalTasks = async () => {
+      const global_tasks = await request(
+        "http://localhost:8080/api/global-task/all/" + id
+      );
+      setGlobalTasks(global_tasks);
     };
     getProject();
+    getGlobalTasks();
   }, [id, request]);
 
-  const global_tasks = useSelector((state) => state.global_tasks.global_tasks);
-
-
-  const global_tasksList = global_tasks?.map((global_task, index) => (
-    <li key={index} className="list-group-item">
-        <GlobalTaskCard id={id} global_taskId={global_task.id} title={global_task.title} description={global_task.description} />
-    </li>
-  ));
+  const global_tasksList =
+    global_tasks.length &&
+    global_tasks.map((global_task, index) => (
+      <li key={index} className="list-group-item">
+        <GlobalTaskCard
+          id={id}
+          global_taskId={global_task.id}
+          title={global_task.title}
+          description={global_task.description}
+        />
+      </li>
+    ));
 
   return (
     <div className="project_detail">
@@ -46,11 +58,16 @@ const ProjectDetail = () => {
       <div className="bock_global_task-list">
         <div className="bock_global_task-list_title mb-2">
           Global tasks
-         <Modal title="Create global task" forElement="create-global_task" component={<GlobalTaskForm project_id={id} />} />
+          <Modal
+            title="Create global task"
+            forElement="create-global_task"
+            component={<GlobalTaskForm project_id={id} />}
+          />
           <PlusSquare
             className="ml-5"
             size="24"
-            data-toggle="modal" data-target="#create-global_task"
+            data-toggle="modal"
+            data-target="#create-global_task"
           />
         </div>
         <ul className="bock_global_task-list_body list-group">
