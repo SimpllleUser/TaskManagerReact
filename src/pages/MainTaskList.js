e1d844493c6dac7b42716a6a2216c0b1f5e87170
Loldf <pecheritsa.vv@gmail.com>
2020-10-16 15:12:50 +0300

    Add last fix

--- src/pages/MainTaskList.js
+++ src/pages/MainTaskList.js
@@ -7,16 +7,18 @@ import { initTasks } from "../store/tasks/actions";
 const MainTaskList = () => {
   const dispatch = useDispatch();
   const id = JSON.parse(localStorage.getItem("project") || '{}').id;
+  const userId = JSON.parse(localStorage.getItem("user")).userId;
   const { request, loading } = useHttp();
   useEffect(() => {
-    const getAllTasksProject = async () => {
+    const getAllData = async () => {
       // ! ADD TRY CATCH AND SHOW TOAST
+       // ! * schema.find( { '_id' : { $in: [1,2,3]} }, function(err,data){});
       const tasks = await request(
         "http://localhost:8080/api/tasks/all_tasks/" + id
       );
       dispatch(initTasks(tasks));
     };
-    getAllTasksProject();
+    getAllData();
   }, [id, request]);
   const tasks = useSelector((state) => state.tasks.tasks);
   return (
