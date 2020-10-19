400dc7d54328800228c5476f9905e1c6647aa6db
Loldf <pecheritsa.vv@gmail.com>
2020-10-19 12:25:39 +0300

    Add last fix

--- src/pages/MainTaskList.js
+++ src/pages/MainTaskList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import TaskList from "../Task/TaskList";
 import { useSelector } from "react-redux";
 const MainTaskList = () => {
