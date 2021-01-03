abfec81bda3cb95a1820cf502eaed633b3ffe407
Vitalii.Pecherytsya <Vitalii.Pecherytsya@rgc.ua>
2021-01-03 15:42:54 +0200

    Refactor MainTasks

--- src/pages/MainTaskList.js
+++ src/pages/MainTaskList.js
@@ -24,6 +24,7 @@ const MainTaskList = () => {
   }, []);
 
   const tasks = useSelector((state) => state?.tasks.allTasks);
+  
   return (
     <div>
       <Pane display="flex" justifyContent="center">
